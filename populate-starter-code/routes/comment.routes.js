01afe1d3c757cfc27aea2c4d2fe7461920f00c27
Nuno Pacheco <nunorpacheco@gmail.com>
2020-08-11 16:24:58 +0100

    done

--- populate-starter-code/routes/comment.routes.js
+++ populate-starter-code/routes/comment.routes.js
@@ -8,7 +8,43 @@ const Comment = require('../models/Comment.model');
 // ****************************************************************************************
 // POST route - create a comment of a specific post
 // ****************************************************************************************
+router.post('/posts/:postId/comment', (req, res)=> {
+    const {postId} = req.params;
+    const {author, content} = req.body;
 
+    User.findOne({username: author})
+    .then(userFromDB => {
+        user = userFromDB
+
+        if(userFromDB == null){
+          return User.create({username: author})
+        }
+
+    })
+    .then( newUser =>{
+        Post.findById(postId)
+        .then(dbPost =>{
+            let newComment;
+
+            if( newUser){
+                newComment = new Comment({author: newUser._id, content})
+            }else{
+                newComment = new Comment({author: user._id, content})
+            }
+
+            newComment
+            .save()
+            .then(dbComment => {
+                dbPost.comments.push(dbComment._id);
+
+                dbPost.save()
+                .then( updatedPost => res.redirect(`/posts/${updatedPost}))
+            })
+
+        })
+    })
+    .catch(err => console.log(`Err while...BB: ${err}`))
+})
 // ... your code here
 
 module.exports = router;
--- populate-starter-code/routes/post.routes.js
+++ populate-starter-code/routes/post.routes.js
@@ -22,20 +22,53 @@ router.get('/post-create', (req, res) => {
 // ****************************************************************************************
 
 // <form action="/post-create" method="POST">
-
-// ... your code here
+router.post('/post-create', (req, res) => {
+  const { title, content, author } = req.body;
+  // 'author' represents the ID of the user document
+  Post.create({ title, content, author })
+    .then(dbPost => {
+      // when the new post is created, the user needs to be found and its posts updated with the
+      // ID of newly created post
+      return User.findByIdAndUpdate(author, { $push: { posts: dbPost._id } });
+    })
+    .then(() => res.redirect('/posts')) // if everything is fine, redirect to list of posts
+    .catch(err => console.log(`Err while creating the post in the DB: ${err}`));
+});
 
 // ****************************************************************************************
 // GET route to display all the posts
 // ****************************************************************************************
 
-// ... your code here
+router.get('/posts', (req, res) => {
+  Post.find()
+    .populate('author') // --> we are saying: give me whole user object with this ID (author represents an ID in our case)
+    .then(dbPosts => {
+      console.log(dbPosts);
+      res.render('posts/list', { posts: dbPosts });
+    })
+    .catch(err => console.log(`Err while getting the posts from the DB: ${err}`));
+});
 
 // ****************************************************************************************
 // GET route for displaying the post details page
 // shows how to deep populate (populate the populated field)
 // ****************************************************************************************
 
-// ... your code here
+router.get('/posts/:postId', (req, res) => {
+  const { postId } = req.params;
+
+  Post.findById(postId)
+    .populate('author comments') // <-- the same as .populate('author).populate('comments')
+    .populate({
+      // we are populating author in the previously populated comments
+      path: 'comments',
+      populate: {
+        path: 'author',
+        model: 'User'
+      }
+    })
+    .then(foundPost => res.render('posts/details', foundPost))
+    .catch(err => console.log(`Err while getting a single post from the  DB: ${err}`));
+});
 
 module.exports = router;
